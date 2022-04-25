4ab5de5eae23501c483600a5aa8f35479240b87c
Peter <74869040+pszemraj@users.noreply.github.com>
2022-04-25 02:42:27 +0200

    :zap: :sparkles: add .7z functionality to enable easily unpacking full data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,11 +6,12 @@ from tqdm.auto import tqdm
 from cleantext import clean
 import sys
 import pandas as pd
-
+import py7zr
+import shutil
 _src = Path(__file__).parent.parent
 _root = _src.parent
 sys.path.append(str(_root.resolve()))
-from src.utils import fix_punct_spaces
+from src.utils import collapse_directory, fix_punct_spaces
 
 
 def process_txt_data(
@@ -59,16 +60,47 @@ def main(
     """
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
+    input_path = Path(input_path)
+    output_path = Path(output_path)
+    if process_zip_file:
+        zipfiles = [f for f in input_path.iterdir() if f.is_file() and f.suffix == ".7z"]
+        logger.info(f"extracting zip files.. found {len(zipfiles)} zip files")
+        temp_dir = input_path / "temp_7z_dir"
+        for f in tqdm(zipfiles, total=len(zipfiles)):
+            with py7zr.SevenZipFile(f, 'r') as z:
+                z.extractall(path=temp_dir)
+        collapse_directory(temp_dir)
+    # extract standard txt files
     csv_paths = process_txt_data(
         txt_datadir=input_path,
         out_dir=output_path,
         lowercase=lowercase,
         verbose=verbose,
     )
+    if process_zip_file:
+        _input_dir_count = len(csv_paths)
+        logger.info(f"processed {_input_dir_count} files in input_path")
+        # extract text files from zip files
+        logger.info(f"extracting zip files in {temp_dir.resolve()}")
+        csv_paths += process_txt_data(
+            txt_datadir=temp_dir,
+            out_dir=output_path,
+            lowercase=lowercase,
+            verbose=verbose,
+        )
+        zip_count = len(csv_paths) - _input_dir_count
+        logger.info(f"processed {zip_count} files in temp_dir")
+
+        # remove temp dir
+        logger.info(f"removing temp dir {temp_dir.resolve()}")
+        shutil.rmtree(temp_dir, ignore_errors=True)
+    logger.info(f"returning {len(csv_paths)} csv files")
     if verbose:
         print(
             f"processed and saved:\n\t{[f.name for f in csv_paths]} in directory {output_path}"
         )
+
+    return csv_paths
 
 
 def get_parser():
@@ -123,7 +155,8 @@ def get_parser():
 
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logging.basicConfig(level=logging.INFO, format=log_fmt,
+                        filename=_root / 'logs' / 'make_dataset.log')
 
     args = get_parser().parse_args()
     logger = logging.info(f"parsed args: {args}")
@@ -136,10 +169,10 @@ if __name__ == "__main__":
     process_zip_file = args.process_zip_file
     lowercase = args.lowercase
     verbose = args.verbose
-    main(
-        input_path=input_dir,
-        output_path=output_dir,
-        lowercase=lowercase,
-        process_zip_file=process_zip_file,
-        verbose=verbose,
-    )
+    _ = main(
+            input_path=input_dir,
+            output_path=output_dir,
+            lowercase=lowercase,
+            process_zip_file=process_zip_file,
+            verbose=verbose,
+        )
